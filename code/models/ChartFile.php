7d7b187baa8b151397011766696135a5b621771d
James Ellis <james.ellis@dpc.nsw.gov.au>
2016-10-07 01:17:09 +0000

    Validate ChartFiles, including by mimetype. If the file is not valid, unlink it from it's path as UploadField copies the file before write()

--- code/models/ChartFile.php
+++ code/models/ChartFile.php
@@ -5,20 +5,39 @@ class ChartFile extends \File {
 		'csv',
 	);
 
-	public function onBeforeWrite() {
-		parent::onBeforeWrite();
-		$ext = $this->getExtension();
-		if($ext != "csv") {
-			throw new ValidationException("Uploaded file needs .csv extension");
+	public function validateWrite() {
+		parent::validateWrite();
+		try {
+			$path = $this->getFullPath();
+			$ext = $this->getExtension();
+			if($ext != "csv") {
+				//\SS_Log::log("Not valid extnsion {$ext}", \SS_Log::DEBUG);
+				throw new \Exception("Uploaded file needs .csv extension");
+			}
+
+			if(class_exists('finfo')) {
+				// in some instances, if Excel is installed on a machine uploading, the mimetype is application/vnd.ms-excel. Crazy.
+				$mimetypes = array("text/csv", "text/plain", "text/x-csv", "application/vnd.ms-excel");
+				$finfo = new \finfo(FILEINFO_MIME_TYPE);
+				$type = $finfo->file($path);
+				//\SS_Log::log("File is of type {$type}", \SS_Log::DEBUG);
+				if(!in_array($type, $mimetypes)) {
+					//\SS_Log::log("Not valid...", \SS_Log::DEBUG);
+					throw new \Exception("Incorrect type - {$type}");
+				}
+			}
+		} catch (\Exception $e) {
+			//\SS_Log::log("Not valid..." . $e->getMessage(), \SS_Log::DEBUG);
+			// remove the file on disk, if it exists
+			//\SS_Log::log("File name is {$this->Name} path is {$path}", \SS_Log::DEBUG);
+			if(file_exists($path) && is_writeable($path)) {
+				unlink($path);
+			}
+			return new \ValidationException(
+				"The uploaded file does not appear to be a valid CSV file",
+				E_USER_WARNING
+			);
 		}
 
-		$path = $this->getFullPath();
-		if(class_exists('finfo_file')) {
-			$finfo = finfo_open(FILEINFO_MIME_TYPE);
-			$type = $finfo->file($finfo, $path);
-			var_dump($type);
-			exit;
-		}
-		return TRUE;
 	}
 }
